b3de7357bde8903348f96e8469dda4e263268e8b
Divyank Shah <divyank.shah.2016@gmail.com>
2022-12-24 23:09:11 -0800

    try build

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const index = (): JSX.Element => {
-  return <div className="font-lexend">index</div>;
+  return <div className="font-lexend">hello world</div>;
 };
 
 export default index;
